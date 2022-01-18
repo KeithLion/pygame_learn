1c1a6b7d9099c2d38c94dea679f2f919571f794d
KeithLion <nortonkiara6@gmail.com>
2022-01-17 17:46:22 -0800

    created the game screen / skeleton

--- pywindow.py
+++ pywindow.py
@@ -1,6 +1,33 @@
 import pygame
+import time
+import random
+from pygame.locals import *
 
-pygame.init()
 
-display_height = 1400
-display_width = 700
+def main():
+    quit = False
+
+    while not quit:
+        window.fill((0, 255, 0))
+
+        keypressed = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quit = True
+
+        pygame.display.update()
+        # .update() or .flip() either works
+
+        clock.tick(25)
+        # tick speed/ frames per second
+
+
+# ********** Initialise & run the game **********
+if __name__ == "__main__":
+    width, height = 800, 600                            # Set screen width,height
+    pygame.init()                                       # Start graphics system
+    pygame.mixer.init()                                 # Start audio system
+    window = pygame.display.set_mode((width, height))   # Create window
+    clock = pygame.time.Clock()                         # Create game clock
+    main()
+    pygame.quit()
